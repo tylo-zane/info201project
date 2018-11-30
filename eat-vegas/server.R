b8bc8f88fd0d5e75e0021d9cbc36e1b17f4f4524
tylo-zane <43626511+tylo-zane@users.noreply.github.com>
2018-11-30 12:53:57 -0800

    updated server

--- eat-vegas/server.R
+++ eat-vegas/server.R
@@ -3,31 +3,35 @@
 # Justin Chan, Nick Zhou, Willa Yang, Tyler Marcinyshyn
 # INFO 201
 # Final Project
-# Eat Vegas - Description 
+# Eat Vegas - Description here
 #
 
-library(shiny)
-library(leaflet)
+  library(shiny)
+  library(leaflet)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-  
-  data <- read.csv("Vegas.csv")
-  
-  # The actual points to be displayed on the map
-  selection <- reactive({
-    result <- filter(data, Shape == input$shape) %>%
-      filter(substring(Date, 1, 2) == paste0(input$month, "/"))
-    return(result)
+  # Define server logic required to draw a histogram
+  shinyServer(function(input, output) {
+
+    data <- read.csv("Vegas.csv")
+
+    # The points to be displayed on the map (TO-DO: Set points to actual restaurants)
+    selection <- reactive({
+      result <- filter(data, categories == input$Cuisine) %>%
+        filter(substring(Date, 1, 2) == paste0(input$month, "/"))
+      return(result)
+    })
+
+    # The map, to be rendered by leafletOutput()
+    output$foodmap <- renderLeaflet({
+      leaflet() %>%
+        addTiles() %>%
+        setView(lng=-115.1, lat=36, zoom=4) %>% # sets initial viewpoint of map
+        addMarkers(lng = selection()$lng, lat = selection()$lat) %>%
+        fitBounds(~min(-115.4), ~min(36), ~max(-115), ~max(36.5)) # latitude and longitude boundaries
+    })
+
+    # "Incremental changes to the map should be performed in
+    # an observer. Each independent set of things that can change
+    # should be managed in its own observer." - shiny & leaflet tutorial
+
   })
-  
-  # The map, to be rendered by leafletOutput()
-  output$foodmap <- renderLeaflet({
-    leaflet() %>%
-      addTiles() %>%
-      setView(lng=-115.1, lat=36, zoom=4) %>% # sets initial viewpoint of map
-      addMarkers(lng = selection()$lng, lat = selection()$lat) %>%
-      fitBounds(~min(-115.4), ~min(36), ~max(-115), ~max(36.5)) # latitude and longitude boundaries
-  })
-  
-})
